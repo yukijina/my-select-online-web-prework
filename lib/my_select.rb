d95c0f31dbc7579f6c83f53c20a559c0bbf829d9
Yuki N <lunarbase@outlook.com>
2018-11-03 15:11:59 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,27 +1,23 @@
 def my_select(collection)
 
- #new_array = []
- 
- collection.select do |col|
-   yield col 
- end
-  
-# i = 0
-#   while i < collection.length
-#     a = yield collection[i]
-#     new_array << a
+ new_array = []
+ i = 0
+  while i < collection.length
+    a = yield collection[i]
+    new_array << a
     
-#     i += 1
-  #end  
-  #new_array.compact
-  col
+     i += 1
+  end  
+  puts new_array.compact
 end
 
-number = [1,2,3,4,5]
+number = [1,2,3,4]
 
 my_select(number) do |num|
    if num % 2 == 0
-     num
-   end
+  num
+  end
 end
+
+
 
