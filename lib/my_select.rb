6326531949affb023d00093a79691e10eabeaac5
Yuki N <lunarbase@outlook.com>
2018-11-03 15:23:26 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,12 +3,12 @@ def my_select(collection)
  new_array = []
  i = 0
   while i < collection.length
-    yield collection[i]
-    new_array << collection[i]
+    a = yield collection[i]
+    new_array << a
     
      i += 1
   end  
-  new_array
+  new_array.compact
 end
 
 number = [1,2,3,4,5]
