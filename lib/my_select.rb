219f2843f075c9ef0476155b21153eb88aeeb1f6
Yuki N <lunarbase@outlook.com>
2018-11-03 15:39:45 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
 def my_select(collection)
- # your code here!
+
+ new_array = []
+i = 0
+  while i < collection.length
+    a = yield collection[i]  
+    # Need to store in variable otherwise, collection[i]= original array(1,2,3,4,5)
+    if a == true 
+      new_array << collection[i]  
+    end
+    i += 1
+  end  
+  new_array
 end
+
+number = [1,2,3,4,5]
+
+my_select(number) do |num|
+  num.even? #even returns true or false
+end
+
+
+
