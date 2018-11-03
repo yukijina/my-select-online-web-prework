c0d61dcf579403eff42c9c2c88a68c53671c7426
Yuki N <lunarbase@outlook.com>
2018-11-03 15:09:29 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,17 @@
 def my_select(collection)
 
  new_array = []
- i = 0
-  while i < collection.length
-    a = yield collection[i]
-    new_array << a
+ 
+ collection.select do |col|
+   yield col 
+ end
+  
+# i = 0
+#   while i < collection.length
+#     a = yield collection[i]
+#     new_array << a
     
-     i += 1
+#     i += 1
   end  
   new_array.compact
 end
