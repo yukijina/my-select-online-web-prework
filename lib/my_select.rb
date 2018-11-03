ee1ac9c1101db84fe4dab452b9e28543f1a8e713
Yuki N <lunarbase@outlook.com>
2018-11-03 15:39:00 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,8 @@ def my_select(collection)
  new_array = []
 i = 0
   while i < collection.length
-    a = yield collection[i]
+    a = yield collection[i]  
+    #Need to store in variable otherwise, collection[i]= original array(1,2,3,4,5)
     if a == true 
       new_array << collection[i]  
     end
